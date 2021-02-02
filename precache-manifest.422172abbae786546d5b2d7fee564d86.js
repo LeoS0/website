4d19dd8d364d48752826a0d1de2872cce40d63fc
Lenti Shala <lenti@AirsomthorLenti.lan>
2021-02-02 15:11:05 +0100

    Upload

---
+++ precache-manifest.422172abbae786546d5b2d7fee564d86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f33d68a097e6774f56",
+    "url": "/website/css/app.e49fa656.css"
+  },
+  {
+    "revision": "d3ca1e39fd2fa0d17a1c8a03c6ee48c5",
+    "url": "/website/img/rotate.png"
+  },
+  {
+    "revision": "5fab87a78aaa6cfb21022710b29d2754",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "d8f33d68a097e6774f56",
+    "url": "/website/js/app.924a6fd6.js"
+  },
+  {
+    "revision": "4a4a4a4813d44d43bb66",
+    "url": "/website/js/chunk-vendors.4cfe16a2.js"
+  },
+  {
+    "revision": "6cf35556693ce873131598eb804a0f93",
+    "url": "/website/json/data.json"
+  },
+  {
+    "revision": "7fc3cef0af61c3201b7fc1b308d36886",
+    "url": "/website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/website/robots.txt"
+  }
+]);
